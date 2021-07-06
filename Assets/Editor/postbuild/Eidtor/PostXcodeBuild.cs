1e1eb77e15d579a3560b3fd155b3b881209679be
JaesungMoon <jsdekiru@gmail.com>
2021-07-07 04:53:51 +0900

    added postbuildprocessor script

---
+++ Assets/Editor/postbuild/Eidtor/PostXcodeBuild.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using UnityEngine;
+
+namespace PostBuild
+{
+    public class PostXcodeBuild
+    {
+        [PostProcessBuild]
+        public static void SetXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+        {
+            Debug.Log(">>> SetXcodePlist");
+            if (buildTarget != BuildTarget.iOS) {
+                Debug.Log("BuildTarget is not iOS buildTarget = " + buildTarget);
+                return;
+            } 
+
+            var plistPath = pathToBuiltProject + "/Info.plist";
+            var plist = new PlistDocument();
+            plist.ReadFromString(File.ReadAllText(plistPath));
+
+            var rootDict = plist.root;
+            // ここに記載したKey-ValueがXcodeのinfo.plistに反映されます
+            rootDict.SetString("GADApplicationIdentifier", "hogehoge");
+
+            File.WriteAllText(plistPath, plist.WriteToString());
+
+            Debug.Log("<<< SetXcodePlist");
+        }
+    }
+}
+
--- readme.md
+++ readme.md
@@ -6,3 +6,20 @@ ios plist update
 
 https://qiita.com/isa130pull/items/cf4642b6df387eade337
 
+[PostProcessBuild]
+
+
+
+## print
+MonoBehaviour.print
+
+public static void print(object message);
+
+## Debug.Log
+UnityEngine
+
+using UnityEngine;
+
+Debug.Log("This is a log message.");
+Debug.LogWarning("This is a warning message!");
+Debug.LogError("This is an error message!");
