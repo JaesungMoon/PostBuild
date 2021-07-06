a2d4272d206cbb507c22e3a2cc3d2c2438157148
JaesungMoon <jsdekiru@gmail.com>
2021-07-07 04:58:52 +0900

    PostBuildPlanB also works

---
+++ Assets/Editor/PostBuildPlanB.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using UnityEngine;
+
+namespace PostBuildPlanB
+{
+    public class PostXcodeBuild
+    {
+        [PostProcessBuild]
+        public static void SetXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+        {
+            Debug.Log(">>> SetXcodePlist PlanB");
+            if (buildTarget != BuildTarget.iOS) {
+                Debug.Log("BuildTarget is not iOS buildTarget = " + buildTarget);
+                return;
+            } 
+
+            var plistPath = pathToBuiltProject + "/Info.plist";
+            var plist = new PlistDocument();
+            plist.ReadFromString(File.ReadAllText(plistPath));
+
+            var rootDict = plist.root;
+            // ここに記載したKey-ValueがXcodeのinfo.plistに反映されます
+            rootDict.SetString("GADApplicationIdentifier", "hogehoge");
+
+            File.WriteAllText(plistPath, plist.WriteToString());
+
+            Debug.Log("<<< SetXcodePlist PlanB");
+        }
+    }
+}
+
